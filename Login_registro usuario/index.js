0b6f9677eaf0aeb4a8017bcbbc8efb4c4ed2ea81
IngridTipacti <ingridtr23@gmail.com>
2018-07-18 09:52:21 -0500

    prueba

---
+++ Login_registro usuario/index.js
@@ -0,0 +1,11 @@
+$('.form'.find('input')).on('keyup blur focus', function (e){
+    var $this = $(this),
+    label = $this.prev('label')
+
+    if (e.type === 'keyup'){
+        if ($this.val() === '')
+        
+    }
+
+
+})
